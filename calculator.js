125c95c56ba46ce65910a7c30ab73f3b9f0bb98a
Ayoola Abraham <abrahamayoola4@gmail.com>
2021-07-28 10:37:11 +0100

    debugged an error and was able to get my calculator working properly

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 //for the history 
 function history() {
-    return document.getElementsByClassName('history-value').innerText();
+    return document.getElementsByClassName('history-value').innerText;
 };
 
 
@@ -15,20 +15,42 @@ function outPut() {
     return document.getElementsByClassName('output-value').innerText;
 };
 
-//output value 
+//output value. This function helps us to 
 function getOutput(number) {
     if (number == '') {
         document.getElementsByClassName('output-value').innerText = number;
     } else {
         document.getElementsByClassName('output-value').innerText = formattedNumber(number);
     }
+}
+
+//formatted number. This function helps us to return a comma within the value.
+function formattedNumber() {
+    var num = Number(number);
+    var value = num.localeString('en');
+    return value;
+}
 
 
-    //formatted number
-    function formattedNumber() {
-        var num = Number(number);
-        var value = num.localeString('en');
-        return value;
-    }
+//Reverse the format. This will help us replace the comma to an empty character
 
-    //     // Reverse our number
+function reverseNumber(number) {
+    return Number(num.replace(/,/g, ''));
+}
+
+//declaring the operator and their ID
+var operator = document.getElementsByClassName('operator');
+for (var i = 0; i < operator.length; i++) {
+    operator[i].addEventListener('click', function() {
+        alert('the operator click ' + this.id);
+    })
+}
+
+//Declaring the variables for the numbers 
+
+var numbers = document.getElementsByClassName('number');
+for (var i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', function() {
+        alert('the number you clicked is ' + this.id);
+    })
+}
